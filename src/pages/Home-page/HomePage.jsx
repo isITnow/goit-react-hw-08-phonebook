12b49d3616e98be991b4635f429f5388f07d711e
Roman Serediuk <box4rom@gmail.com>
2022-09-30 18:11:35 +0300

    homepage render upd

--- src/pages/Home-page/HomePage.jsx
+++ src/pages/Home-page/HomePage.jsx
@@ -1,24 +1,42 @@
 import s from './homePage.module.css';
 
 import { useSelector } from 'react-redux';
-import { selectIsLoggedIn, selectUserName } from 'redux/auth/selector-auth';
+import {
+    selectIsLoggedIn,
+    selectUserName,
+    selectToken,
+} from 'redux/auth/selector-auth';
 import { TbNotebook } from 'react-icons/tb';
+import { ThreeDots } from 'react-loader-spinner';
 
 const HomePage = () => {
     const userName = useSelector(selectUserName);
     const isLoggedIn = useSelector(selectIsLoggedIn);
+    const tokenStatus = useSelector(selectToken);
 
     return (
         <div className={s.home__page}>
-            {isLoggedIn ? (
-                <div className={s.box}>
-                    <h2 className={s.title}>{`Welcome, ${userName}!`}</h2>
-                    <p className={s.text}>
-                        <TbNotebook size={23} />
-                        open <b>Contacts</b> tab to see your personal list of
-                        names and phone numbers of people and businesses
-                    </p>
-                </div>
+            {tokenStatus ? (
+                isLoggedIn ? (
+                    <div className={s.box}>
+                        <h2 className={s.title}>{`Welcome, ${userName}!`}</h2>
+                        <p className={s.text}>
+                            <TbNotebook size={23} />
+                            open <b>Contacts</b> tab to see your personal list
+                            of names and phone numbers of people and businesses
+                        </p>
+                    </div>
+                ) : (
+                    <ThreeDots
+                        height="80"
+                        width="80"
+                        radius="9"
+                        color="#5252f8"
+                        ariaLabel="three-dots-loading"
+                        wrapperStyle={{}}
+                        visible={true}
+                    />
+                )
             ) : (
                 <div className={s.box}>
                     <h2 className={s.title}>
